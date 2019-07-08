4cf9094ad07a1ea56ecc9467502145f4fdd1b7b4
Moamen <moamenmohamedalii@gmail.com>
2019-07-08 03:13:15 +0200

    Store extracted data in mongoDB

--- FreelancerScraper/spiders/JobsSpider.py
+++ FreelancerScraper/spiders/JobsSpider.py
@@ -1,33 +1,40 @@
 import scrapy
 from urllib.parse import urlencode , urlparse
+import pymongo
+from pymongo import ReplaceOne
 
 class JobsSpider(scrapy.Spider):
     name = 'jobs'
 
-    def start_requests(self):
-        base_url = 'http://www.freelancer.com/jobs/690/?'
+    def __init__(self):
+        # Connect to db
+        self.mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
+        self.jobs_collection = self.mongo_client['freelancer']['jobs']
 
-        # constant query strings
+    def start_requests(self):
+        base_url = 'http://www.freelancer.com/jobs/1/?'
+
+        # Constant query strings
         base_url += 'languages=en&status=all'
 
-        # available filters
+        # Available filters
         job_types = ['local','featured','recruiter','fulltime']
         budgets = ['fixed','hourly','contest']
         min_budget = 0
         max_budget = 1000 #for testing only target -> 1000000
         step = 1000
         
-        # create urls with query strings combinations
+        # Create urls with query strings combinations
         urls = []
         for job_type in job_types:
             for budget in budgets:
-                # add job type and buget filters to QS
+                # Add job type and buget filters to QS
                 query_string = {
                     job_type : 'true',
                     budget : 'true'
                 }
 
-                # ranges for budget
+                # Ranges for budget
                 if budget == 'fixed':
                     for i in range(min_budget,max_budget,step):
                         query_string['fixed_min'] = i
@@ -46,16 +53,17 @@ class JobsSpider(scrapy.Spider):
                         query_string['contest_max'] = i + step
                         urls.append(base_url + '&' +urlencode(query_string))
 
-        # save urls to external file
+        # Save urls to external file
         print(urls, file=open('urls.txt', 'w'))
 
-        # send requests
+        # Send requests
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
-            break
+            break # for testing only
 
     def parse(self, response):
         # Extract data fields from page
+        operations = []
         for job in response.css('div.JobSearchCard-item'):
             title = job.css('a.JobSearchCard-primary-heading-link::text').get().strip()
             description = job.css('p.JobSearchCard-primary-description::text').get().strip()
@@ -64,7 +72,21 @@ class JobsSpider(scrapy.Spider):
             bid = job.css('div.JobSearchCard-primary-price::text').get().strip()
             verified = True if job.css('div.JobSearchCard-primary-heading-status').get() else False
 
-            print((title,description,skills,days_left,bid,verified) , '\n' ,  file=open('data.txt', 'a'))
+            # Create a dictionary
+            document = {
+                'title'       : title,
+                'description' : description,
+                'skills'      : skills,
+                'days_left'   : days_left,
+                'bid'         : bid,
+                'verified'    : verified
+            }
+
+            # Add to array
+            operations.append(ReplaceOne(document , document , upsert=True))
+
+        # Bulk insert extracted data from page(avoiding insertion of duplicates)
+        self.jobs_collection.bulk_write(operations)
 
         # Go to next page and repeat till finishing this filter
         next_page_relative_url = response.css('a.Pagination-item::attr(href)')[-2].get()
@@ -73,8 +95,10 @@ class JobsSpider(scrapy.Spider):
             to_follow_url = next_page_relative_url + '?' + query_string
             yield response.follow(to_follow_url, callback=self.parse)
 
-
     def getDaysLeft(self, string):
         days = str.split(string)[0]
-
         return 0 if days == 'Ended' else int(days)
+
+    def closed(self, reason):
+        # Close mongoDB client
+        self.mongo_client.close()
